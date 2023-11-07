9e3b51b0d5d01c91bbea4cb00af8445c61a57e83
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-07 16:32:33 +0100

    salvataggio

---
+++ main.js
@@ -0,0 +1,30 @@
+const listaDellaSpesa = [
+    "acqua",
+    "olio",
+    "linguine",
+    "guanciale",
+    "uova"
+];
+
+const contenitore = document.getElementById("listaSpesa");
+
+
+
+let i = 0;
+
+
+while(i < listaDellaSpesa.length) {
+    
+    const element = listaDellaSpesa[i];
+    const elementHTML = document.createElement("li");
+    elementHTML.innerHTML = element;
+    contenitore.append(elementHTML);
+    i++;
+}
+document.getElementById("addBtn").addEventListener("click", function() {
+    const nuovoElemento = document.getElementById("newElement").value;
+    listaDellaSpesa.push(nuovoElemento);
+    contenitore.innerHTML += `<li>${nuovoElemento}</li>`;
+    document.getElementById("newElement").value = "";
+
+});
